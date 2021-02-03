eddeb1727df6b8bcb288fc18ba9c02167a59b1ca
Wasan Chuenlerdsakun <wasan.tora@gmail.com>
2021-02-03 23:35:44 +0700

    001_Create_Database

---
+++ 001_Create_Database/001_Create_Database/AddDatabase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace _001_Create_Database
+{
+    class AddDatabase
+    {
+        public static void InitializeDatabase()
+        {
+            
+            using (SqliteConnection db =
+               new SqliteConnection("Filename=TestFirstDatabase_DB.db"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS MyTable_TB (Primary_Key INTEGER PRIMARY KEY, " +
+                    "Text_Entry NVARCHAR(2048) NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+    }
+}
